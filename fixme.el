6c58886479a498d2cafce20cf626822b8df21b93
John SJ Anderson <genehack@genehack.org>
2009-06-05 11:13:41 -0400

    fixme

---
+++ fixme.el
@@ -0,0 +1,34 @@
+;;; FIXME HIGHLIGHTING
+
+;; $Id: fixme.el 1 2007-11-26 00:45:09Z genehack $
+;; $URL$
+
+;; modified from <http://ch.twi.tudelft.nl/~mostert/fixme.el>
+(defvar fixme-modes '(emacs-lisp-mode
+                      cperl-mode
+                      )
+  "modes to activate fixme comments in")
+
+(defvar fixme-regexp  "FIXME" )
+
+;; angry red on yellow background. It *really* gets your attention
+(defface font-lock-fixme-face
+  '((((class grayscale) (background light)) (:foreground "DimGray" :italic t))
+    (((class grayscale) (background dark))  (:foreground "LightGray" :italic t))
+    (((class color)     (background light)) (:foreground "Red"
+                                             :background "Yellow"))
+    (((class color)     (background dark))  (:foreground "Red"
+                                             :background "Yellow")))
+  "face for fixme comments. It should *really* get your attention")
+
+(defvar font-lock-fixme-face 'font-lock-fixme-face)
+
+(mapc
+ (lambda (mode)
+   (font-lock-add-keywords
+    mode
+    (list (list fixme-regexp 0 font-lock-fixme-face t))))
+ fixme-modes)
+
+
+(provide 'fixme)
