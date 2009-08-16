330bf96d5ba6c8c0dd8c30353982ad77d76563b7
John SJ Anderson <genehack@genehack.org>
2009-08-16 09:48:41 -0400

    merge in changes from http://tweedle-dee.org/svn/emacs.d/site-lisp/yaml-mode.el

--- yaml-mode.el
+++ yaml-mode.el
@@ -112,7 +112,7 @@ that key is pressed to begin a block literal."
 (defconst yaml-blank-line-re "^ *$"
   "Regexp matching a line containing only (valid) whitespace.")
 
-(defconst yaml-comment-re "\\(#*.*\\)"
+(defconst yaml-comment-re "\\(#+.*\\)"
   "Regexp matching a line containing a YAML comment or delimiter.")
 
 (defconst yaml-directive-re "^\\(?:--- \\)? *%\\(\\w+\\)"
@@ -218,7 +218,9 @@ that key is pressed to begin a block literal."
   (set (make-local-variable 'font-lock-defaults)
        '(yaml-font-lock-keywords
          nil nil nil nil
-         (font-lock-syntactic-keywords . yaml-font-lock-syntactic-keywords))))
+         (font-lock-syntactic-keywords . yaml-font-lock-syntactic-keywords)))
+  ;; TABs are not allowed in YAML
+  (set (make-local-variable 'indent-tabs-mode) nil))
 
 
 ;; Font-lock support
